21fb0db52ed6dae3c2f0b2a296d3dfb670bb1529
jshugart90-png <jshugart90@gmail.com>
2026-04-21 15:17:31 -0500

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
+
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: '(tabs)', // This tells the router where your tabs live
 };
 
 export default function RootLayout() {
